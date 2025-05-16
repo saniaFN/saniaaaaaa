1abf43470156ea84a0cf8088166040787920d43a
saniaFN <saniafebriani14@gmail.com>
2025-05-16 19:20:44 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,14 +2,14 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1f365ccd18f180d2905df1d7ab0b238d',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'saniaaaaaa',
   domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
   author: 'Travis Fischer',
 
